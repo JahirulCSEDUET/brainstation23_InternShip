247b5739d99b267cbfdd44e3e5bab4b542e09ec1
MD Jahirul Islam <jahirulcseduet@gmail.com>
2026-05-23 02:25:45 +0600

    Violation and Following

--- SolidPrinciple/Following/F4InterfaceSegregationPrinciple.cs
+++ SolidPrinciple/Following/F4InterfaceSegregationPrinciple.cs
@@ -1,5 +1,40 @@
 ﻿
+using SolidPrinciple.Violation;
+
 namespace SolidPrinciple.Following
 {
-    
+    public interface IWorkable
+    {
+        void Work();
+    }
+    public interface IEatable
+    {
+        void Eat();
+    }
+    public interface ISleepable
+    {
+        void Sleep();
+    }
+    public class Human : IWorkable, ISleepable, IEatable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Working");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping");
+        }
+    }
+    public class Robot : IWorkable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Working.");
+        }
+    }
 }
--- SolidPrinciple/Violation/V4InterfaceSegregationPrinciple.cs
+++ SolidPrinciple/Violation/V4InterfaceSegregationPrinciple.cs
@@ -1,4 +1,42 @@
 ﻿
 namespace SolidPrinciple.Violation
 {
+    public interface IWorker
+    {
+        void Work();
+        void Eat();
+        void Sleep();
+    }
+    public class Human : IWorker
+    {
+        public void Work() 
+        { 
+            Console.WriteLine("Working"); 
+        }
+        public void Eat() 
+        { 
+            Console.WriteLine("Eating");
+        }
+        public void Sleep() 
+        {
+            Console.WriteLine("Sleeping");
+        }
+    }
+    public class Robot : IWorker
+    {
+        public void Eat()
+        {
+            throw new InvalidOperationException("Robot Cannot be eat.");
+        }
+
+        public void Sleep()
+        {
+            throw new InvalidOperationException("Robot Cannot be sleep.");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Working");
+        }
+    }
 }
