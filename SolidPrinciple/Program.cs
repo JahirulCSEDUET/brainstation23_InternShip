8a92caf5d45b06abdf6cf4284c7e3a6319b85ea9
MD Jahirul Islam <jahirulcseduet@gmail.com>
2026-05-23 01:15:27 +0600

    Following and Violation

--- SolidPrinciple/Program.cs
+++ SolidPrinciple/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SolidPrinciple.Following;
+
+var alerts = new List<INotification>
+{
+    new EmailNotification
+    {
+        EmailAddress="jahirulcseduet@gmail.com",
+        Subject="System alert",
+        Message ="Server is down!"
+    },
+    new SmsNotification
+    {
+        PhoneNumber ="+8801626467279",
+        Message ="Server is down!"
+    },
+    new PushNotification
+    {
+        DeviceToken="jdsxchsnls",
+        Message="Server is down!"
+    }
+};
+foreach(var alert in alerts)
+{
+    alert.send();
+}
