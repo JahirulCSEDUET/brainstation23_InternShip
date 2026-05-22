f32ede62b02e01f0efa53f0f9ee1dfd295e14bf2
MD Jahirul Islam <jahirulcseduet@gmail.com>
2026-05-22 19:31:35 +0600

    Violation

--- SolidPrinciple/Violation/V2OpenClosedPrinciple.cs
+++ SolidPrinciple/Violation/V2OpenClosedPrinciple.cs
@@ -1,5 +1,33 @@
 ﻿
 namespace SolidPrinciple.Violation
 {
-    
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+    public class Circle
+    {
+        public double Radius { get; set; }
+    }
+    public class AreaCalculator
+    {
+        public double TotalArea(object[] shapes)
+        {
+            double totalArea = 0.0;
+            foreach (var shape in shapes)
+            {
+                if (shape is Rectangle r)
+                {
+                    totalArea += r.Width * r.Height;
+                }
+                else if (shape is Circle c)
+                {
+                    totalArea += Math.PI * c.Radius * c.Radius;
+                }
+                //When a new shape Triangle added tomorrow than this method must be modified.
+            }
+            return totalArea;
+        }
+    }
 }
