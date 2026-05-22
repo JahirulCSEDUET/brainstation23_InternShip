cd52a99ca3d1f59959c54c6ffe796c1f943452e4
MD Jahirul Islam <jahirulcseduet@gmail.com>
2026-05-23 04:14:26 +0600

    Following and Violation

--- SolidPrinciple/Program.cs
+++ SolidPrinciple/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SolidPrinciple.Following;
+
+ILogger logger= new FileLogger();
+OrderService localLogger = new OrderService(logger);
+localLogger.CreateOrder(236);
+
+ILogger logger1 = new CloudLogger();
+OrderService globalLogger = new OrderService(logger1);
+localLogger.CreateOrder(267);
